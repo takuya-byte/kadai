73e79f94a2a3f149a727f0ca7fa1a38bf5369605
takuya.okita <trainingdayokita@gmail.com>
2020-09-07 09:11:41 +0000

    kadai3-3

--- kadai/app/Todo.php
+++ kadai/app/Todo.php
@@ -6,22 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-
-const STATUS = [
-    
-false => [ 'label' => ' 完了' ],
-true => [ 'label' => ' 作業中' ], 
-];
-
-public function getStatusLabelAttribute()
-{
-    
-    $status = $this->attributes['status'];
-    
-    if(!isset(self::STATUS[$status])){
-        return '';
-    }
-    return self::STATUS[$status]['label'];
-}
-
+  protected $table = 'todos';
+  protected $fillable = ['id', 'body', 'status'];
+  public static $rules = array(
+    'body' => 'required'
+  );
 }
